72140c396a44cfc856310bfa45ca9636919e63d3
yura <yurcin026@gmail.com>
2022-04-04 01:17:24 +0700

    mobile hueta

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup>
 </script>
 <template>
-  <main>
+<main v-if="isMobile()">
+<h1>Mobile</h1>
+</main>
+  <main v-else>
     <div class="annotation">Данный сайт поможет новичкам в игре Genshin Impact собрать отряд из определленных<br>
       персонажей, продемонстрирует их способности и материалы для прокачки.
       <br>
@@ -21,6 +24,15 @@ export default {
   name: "Home",
   components: {
     GalleryBlock
+  },
+  methods: {
+    isMobile() {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true
+      } else {
+        return false
+      }
+    }
   }
 }
 </script>
